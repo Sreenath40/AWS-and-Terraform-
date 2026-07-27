6a74fecf0aa853536898ff891639a2ac7c120326
Sreenath40 <103664141+Sreenath40@users.noreply.github.com>
2026-07-27 09:36:39 +0530

    Update main.tf

--- Project2/main.tf
+++ Project2/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "allow_ssh_http" {
 }
 
 resource "aws_instance" "web_server" {
-  ami                    = "ami-0b6d9d3d33ba97d99"
+  ami                    = "ami-004f790b835b26145"
   instance_type          = "t3.micro"
   key_name               = "terraform-key"
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
